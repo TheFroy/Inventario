d8817be9f3b7372d343f633c2492b68e7b05ecc9
TheFroy <jarodf_11@hotmail.com>
2020-12-16 13:04:28 -0500

    both connections

--- db/conexion.php
+++ db/conexion.php
@@ -6,12 +6,6 @@ server with default setting (user 'root' with no password) */
 // define('DB_PASSWORD', 'rtJ4e1PEt36eq0Ye8MkU');
 // define('DB_NAME', 'bd9w7cm5sj0bqjc2hsxz');
 
-// /* Attempt to connect to MySQL database */
-// $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-// // Check connection
-// if($link === false){
-//     die("ERROR: Could not connect. " . mysqli_connect_error());
-// }
 
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'superUser');
